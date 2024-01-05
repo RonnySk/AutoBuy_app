e4aea5008acf60bf4ed679ac37cdcb8e26822c7c
RonnySk <ronny_sk@hotmail.com>
2024-01-05 11:18:17 +0100

    add vehiclecardlist

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -4,15 +4,15 @@ import * as React from "react";
 import { useEffect, useState } from "react";
 import AdCard from "@src/app/components/AdCard";
 
-const PromptCardList = ({ adData }: Advertisement[]) => {
-  return (
-    <div className="mt-16 space-y-6 py-8 sm:columns-2 sm:gap-6 xl:columns-3 ">
-      {adData.length !== 0
-        ? adData.map((ad: Advertisement) => <AdCard key={ad._id} adData={ad} />)
-        : null}
-    </div>
-  );
-};
+// const PromptCardList = ({ adData }: Advertisement[]) => {
+//   return (
+//     <div className="mt-16 space-y-6 py-8 sm:columns-2 sm:gap-6 xl:columns-3 ">
+//       {adData.length !== 0
+//         ? adData.map((ad: Advertisement) => <AdCard key={ad._id} adData={ad} />)
+//         : null}
+//     </div>
+//   );
+// };
 
 const Feed = () => {
   const [ads, setAds] = useState<Advertisement[]>([]);
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,27 @@
-import AddVehicleForm from "../components/AddVehicleForm";
+import { useEffect, useState } from "react";
 
 const Profile = () => {
+  const [ads, setAds] = useState<Advertisement[]>([]);
+
+  useEffect(() => {
+    const fetchAds = async () => {
+      const response = await fetch("/api/advertisement/all", {
+        method: "GET",
+      });
+      const data = await response.json();
+      setAds(data);
+    };
+
+    fetchAds();
+  }, []);
+  console.log("ads state", ads);
+
+  return (
+    <section>
+      <PromptCardList adData={ads} />
+    </section>
+  );
+
   return (
     <>
       <div>profile</div>
